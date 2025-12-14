0a5b6bae313fd7a2e2b367c3a57500df4505e6ff
Meet Dharmesh Gandhi <meetgandhi13062005@gmail.com>
2025-12-14 19:03:56 +0530

    feat: created the experience section

---
+++ frontend/src/lib/components/home/Experience.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+	import "$lib/components/home/experience.css";
+
+	interface Experience {
+		role: string,
+		org?: string;
+		description?: string;
+		redirect?: string;
+	}
+
+    let experiences: Experience[] = [
+        {
+            role: 'Robotics Head',
+            org: 'IEEE',
+            description: 'Led and organized competitive robotics events in collaboration with multiple IEEE teams and branches.',
+			redirect: '/',
+		},
+        {
+            role: 'Web Development Intern',
+            org: 'Club Duelz',
+            description: 'Contributed feature ideas and implemented them through collaborative brainstorming and development.',
+			redirect: '/',
+		},
+    ];
+</script>
+
+<section class="flex flex-col justify-center items-center">
+	<h1 class="text-(--green)">Experience</h1>
+	<article class="grid grid-cols-2 gap-8 p-8">
+		{#each experiences as experience}
+			<div
+				class="flex flex-col bg-(--blue) rounded-xl w-fit p-8 text-(--black) border-4 g-dg-btg-md-border"
+			>
+				<h1>{experience.role}</h1>
+				{#if experience.org}
+					<h2>{experience.org}</h2>
+				{/if}
+				{#if experience.description}
+					<p>{experience.description}</p>
+				{/if}
+				<a
+					href={experience.redirect ?? '/'}
+					class="view-experience-btn flex justify-center items-center self-center gap-4 bg-(--dark-blue) rounded-md text-(--white) w-fit px-4 cursor-pointer mt-8"
+				>
+					Know more about this experience
+					<span class="text-2xl"> &rarr; </span>
+				</a>
+			</div>
+		{/each}
+	</article>
+</section>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script>
 	import Hero from "$lib/components/home/Hero.svelte";
 	import Intro from "$lib/components/home/Intro.svelte";
+	import Experience from "$lib/components/home/Experience.svelte";
 </script>
 
 <main>
 	<Hero />
 	<Intro />
+	<Experience />
 </main>
